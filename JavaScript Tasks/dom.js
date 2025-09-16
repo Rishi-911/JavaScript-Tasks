8256b8cfde6ca541c662ae8c0b0b372840370f3f
Rishi Shringi <rishishringi913@gmail.com>
2025-09-16 17:06:59 +0530

    Dom bg color change

---
+++ JavaScript Tasks/dom.js
@@ -0,0 +1,8 @@
+const handleClick = () => {
+  let backgroud = document.querySelector(".body-class");
+  backgroud.style.backgroundColor = "red";
+};
+const handleClick1 = () => {
+  let backgroud = document.querySelector(".body-class");
+  backgroud.style.backgroundColor = "white";
+};
