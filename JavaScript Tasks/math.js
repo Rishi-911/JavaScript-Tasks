7a63082af24107b16829555b1de2d4b0f5a21b7b
Rishi Shringi <rishishringi913@gmail.com>
2025-09-17 15:31:46 +0530

    math module

---
+++ JavaScript Tasks/math.js
@@ -0,0 +1,11 @@
+let a = Math.PI;
+console.log(a);
+
+let b = Math.floor(Math.random() * 10) + 1;
+console.log(b);
+
+console.log(Math.sqrt(16));
+
+console.log(Math.ceil(12.2));
+
+console.log(Math.pow(2,3));
